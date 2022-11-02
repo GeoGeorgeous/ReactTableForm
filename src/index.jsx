1127770456ffca039e368c7101a49af44aa06bfe
Георгий Горчев <georgegorchev@yandex.ru>
2022-11-02 16:16:27 +0200

    add `prop-types`

---
+++ src/index.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable */
+/* eslint disabled here for module.hot.accept(); */
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './style.sass';
+import App from './components/App/App';
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+module.hot.accept();
